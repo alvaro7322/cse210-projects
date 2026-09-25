ba509f346b9d3c684fa2351cdf978f08380a568b
alvaro7322 <alvaro732255@gmail.com>
2026-09-24 21:59:26 -0500

    week 04 programs

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -1,9 +1,39 @@
 using System;
-
+ 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+ 
+        Address address1 = new Address("123 Main St", "Springfield", "IL", "USA");
+        Customer customer1 = new Customer("Alice Johnson", address1);
+ 
+        Order order1 = new Order(customer1);
+        order1.AddProduct(new Product("Wireless Mouse", "P001", 25.50, 2));
+        order1.AddProduct(new Product("Keyboard", "P002", 45.00, 1));
+        order1.AddProduct(new Product("USB Cable", "P003", 8.75, 3));
+ 
+        Address address2 = new Address("45 King Street", "Toronto", "Ontario", "Canada");
+        Customer customer2 = new Customer("Michael Smith", address2);
+ 
+        Order order2 = new Order(customer2);
+        order2.AddProduct(new Product("Desk Lamp", "P004", 30.00, 1));
+        order2.AddProduct(new Product("Notebook", "P005", 3.25, 5));
+ 
+        Console.WriteLine("Order 1");
+        Console.WriteLine("Packing Label:");
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine("Shipping Label:");
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine("Total Cost: $" + order1.GetTotalCost());
+        Console.WriteLine();
+ 
+        Console.WriteLine("Order 2");
+        Console.WriteLine("Packing Label:");
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine("Shipping Label:");
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine("Total Cost: $" + order2.GetTotalCost());
     }
 }
--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,46 @@
 using System;
-
+using System.Collections.Generic;
+ 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+ 
+        Video video1 = new Video("Learning C# Basics", "Code Academy", 620);
+        video1.AddComment(new Comment("John", "This helped me a lot, thanks!"));
+        video1.AddComment(new Comment("Maria", "Great explanation."));
+        video1.AddComment(new Comment("David", "Can you make a part 2?"));
+ 
+        Video video2 = new Video("Top 10 Travel Spots", "World Explorer", 845);
+        video2.AddComment(new Comment("Sara", "Adding these to my bucket list!"));
+        video2.AddComment(new Comment("Luis", "Number 5 is amazing."));
+        video2.AddComment(new Comment("Anna", "Which camera do you use?"));
+        video2.AddComment(new Comment("Tom", "Great video as always."));
+ 
+        Video video3 = new Video("Easy Pasta Recipe", "Chef Mike", 300);
+        video3.AddComment(new Comment("Karen", "Tried this last night, delicious!"));
+        video3.AddComment(new Comment("Peter", "How long does it take to prepare?"));
+        video3.AddComment(new Comment("Lucy", "Can I use a different sauce?"));
+ 
+        List<Video> videos = new List<Video>();
+        videos.Add(video1);
+        videos.Add(video2);
+        videos.Add(video3);
+ 
+        foreach (Video video in videos)
+        {
+            Console.WriteLine("Title: " + video.GetTitle());
+            Console.WriteLine("Author: " + video.GetAuthor());
+            Console.WriteLine("Length: " + video.GetLength() + " seconds");
+            Console.WriteLine("Number of comments: " + video.GetNumberOfComments());
+ 
+            foreach (Comment comment in video.GetComments())
+            {
+                Console.WriteLine("  " + comment.GetCommenterName() + ": " + comment.GetText());
+            }
+ 
+            Console.WriteLine();
+        }
     }
 }
